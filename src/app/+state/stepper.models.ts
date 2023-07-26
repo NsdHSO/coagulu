ea8f1cea97a852c9c2b3a5c365a2f888cafe2a34
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-26 19:06:46 +0300

    chore:🌻 added model for developer tools

--- src/app/+state/stepper.models.ts
+++ src/app/+state/stepper.models.ts
@@ -2,6 +2,40 @@
  * Interface for the 'Stepper' data
  */
 export interface StepperEntity {
-  id: string | number; // Primary ID
-  name: string;
+    intro: {
+        check: boolean
+        id: number
+    }
+    infoUser: {
+        check: boolean
+        id: number
+        values: {
+            familyNumber: number,
+            roleOfMember: [{
+                id: number,
+                value: string,
+                check: boolean
+            }],
+            childrens: number
+        }
+    },
+    spendMoney: {
+        check: boolean
+        id: number
+        values: {
+            incoming: number,
+            spendMoney: number,
+            revenue: number,
+            categorySpendMoney: [{
+                id: number
+                value: number,
+                label: string,
+                scope: [{
+                    label: string,
+                    check: boolean
+                    id: number
+                }]
+            }]
+        }
+    },
 }
