1e804138fdf7b7883a81c163e77b73f24eef8f4f
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-31 08:23:52 +0300

    test:🐳 all test work

--- src/app/+state/stepper.reducer.spec.ts
+++ src/app/+state/stepper.reducer.spec.ts
@@ -1,41 +1,19 @@
-import { Action } from '@ngrx/store';
-
 import * as StepperActions from './stepper.actions';
-import { StepperEntity } from './stepper.models';
 import {
   initialStepperState,
   stepperReducer,
   StepperState,
 } from './stepper.reducer';
-import { mockStepper } from './mock';
 
 describe('Stepper Reducer', () => {
-  const createStepperEntity = (id: string, name = ''): StepperEntity => ({
-    ...mockStepper(),
-  });
-
   describe('valid Stepper actions', () => {
     it('loadStepperSuccess should return the list of known Stepper', () => {
-      const stepper = [
-        createStepperEntity('PRODUCT-AAA'),
-        createStepperEntity('PRODUCT-zzz'),
-      ];
-      const action = StepperActions.loadStepperSuccess({ stepper });
+      const result: StepperState = stepperReducer(
+        initialStepperState,
+        StepperActions.initStepper
+      );
 
-      const result: StepperState = stepperReducer(initialStepperState, action);
-
-      expect(result.loaded).toBe(true);
-      expect(result.ids.length).toBe(2);
-    });
-  });
-
-  describe('unknown action', () => {
-    it('should return the previous state', () => {
-      const action = {} as Action;
-
-      const result = stepperReducer(initialStepperState, action);
-
-      expect(result).toBe(initialStepperState);
+      expect({ ...(result.entities['infoUser'] as any) }.check).toBe(true);
     });
   });
 });
--- src/app/+state/stepper.selectors.spec.ts
+++ src/app/+state/stepper.selectors.spec.ts
@@ -1,5 +1,10 @@
-import { StepperPartialState } from './stepper.reducer';
+import {
+  initialStepperState,
+  stepperAdapter,
+  StepperPartialState,
+} from './stepper.reducer';
 import { mockStepper } from './mock';
+import * as StepperSelectors from './stepper.selectors';
 
 describe('Stepper Selectors', () => {
   const ERROR_MSG = 'No Error Available';
@@ -8,20 +13,28 @@ describe('Stepper Selectors', () => {
   let state: StepperPartialState;
 
   beforeEach(() => {
-    // state = {
-    //   stepper: stepperAdapter.setAll(
-    //     [
-    //       createStepperEntity('PRODUCT-AAA'),
-    //       createStepperEntity('PRODUCT-BBB'),
-    //       createStepperEntity('PRODUCT-CCC'),
-    //     ],
-    //     {
-    //       ...initialStepperState,
-    //       selectedId: 'PRODUCT-BBB',
-    //       error: ERROR_MSG,
-    //       loaded: true,
-    //     }
-    //   ),
-    // };
+    state = {
+      stepper: stepperAdapter.setAll(
+        [
+          createStepperEntity('PRODUCT-AAA'),
+          createStepperEntity('PRODUCT-BBB'),
+          createStepperEntity('PRODUCT-CCC'),
+        ],
+        {
+          ...initialStepperState,
+          selectedId: 'PRODUCT-BBB',
+          error: ERROR_MSG,
+          loaded: true,
+        }
+      ),
+    };
+  });
+
+  describe('Stepper Selectors', () => {
+    it('selectAllStepper() should return the list of Stepper', () => {
+      const results = StepperSelectors.selectAllStepper(state);
+
+      expect([...results].length).toBe(1);
+    });
   });
 });
