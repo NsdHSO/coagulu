28e4ce55152e5e789dd0dbe31651bf365d9d366b
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-31 14:56:42 +0300

    test:🐳 add injection context

--- src/app/components/stepper-steps/stepper-steps.component.spec.ts
+++ src/app/components/stepper-steps/stepper-steps.component.spec.ts
@@ -1,21 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import * as ang from '@angular/core';
 import { StepperStepsComponent } from './stepper-steps.component';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { of } from 'rxjs';
 
 describe('StepperStepsComponent', () => {
-  let component: StepperStepsComponent;
-  let fixture: ComponentFixture<StepperStepsComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [StepperStepsComponent]
+  const spyInject = jest.spyOn(ang, 'inject');
+  const activate = {};
+  const store = {
+    select: () => of(''),
+  };
+  const setup = (activated: unknown, store: unknown) => {
+    spyInject.mockImplementation((providerToken: unknown) => {
+      if (providerToken === ActivatedRoute) {
+        return activated;
+      }
+      if (providerToken === Store) {
+        return store;
+      }
+      return;
     });
-    fixture = TestBed.createComponent(StepperStepsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    return new StepperStepsComponent();
+  };
+
+  it('should init', () => {
+    const stepperComponent = setup(activate, store);
+
+    expect(stepperComponent).toBeTruthy();
   });
 });
