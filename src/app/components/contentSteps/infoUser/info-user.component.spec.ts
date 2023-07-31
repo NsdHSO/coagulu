2caa3dc3aafe4fb73670d0a41f693c2ebc699dc7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-31 09:05:33 +0300

    chore:🌻 fix typo

--- src/app/components/contentSteps/infoUser/info-user.component.spec.ts
+++ src/app/components/contentSteps/infoUser/info-user.component.spec.ts
@@ -6,10 +6,10 @@ import { of } from 'rxjs';
 describe('InfoUserComponent', () => {
   const spyInject = jest.spyOn(angularCore, 'inject');
 
-  const setup = (stepperSerivce: unknown) => {
+  const setup = (stepperService: unknown) => {
     spyInject.mockImplementation((providerToken: unknown) => {
       if (providerToken === StepperService) {
-        return stepperSerivce;
+        return stepperService;
       }
       return;
     });
