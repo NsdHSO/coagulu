a2d6ad29f59db20234c7598a183ac6dba49c72d2
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-08-17 23:30:12 +0300

    feat:🎸 export all members

--- ftx-forms/src/index.ts
+++ ftx-forms/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/ftx-forms/service';
 export * from './lib/ftx-forms/interfaces';
 export * from './lib/ftx-forms/components';
 export * from './lib/ftx-forms/utils';
+export * from './lib/ftx-forms/shared/';
---
+++ ftx-forms/src/lib/ftx-forms/shared/index.ts
@@ -0,0 +1,3 @@
+export * from './button/button.component';
+export * from './input/input.component';
+export * from './input/pap-control.directive';
