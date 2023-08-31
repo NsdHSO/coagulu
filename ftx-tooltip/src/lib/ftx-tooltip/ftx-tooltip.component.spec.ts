fab9aab11222b4edfbdef235ebe0e01b133e2603
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-08-31 10:16:40 +0300

    test:🐳 tooltip

--- ftx-tooltip/src/lib/ftx-tooltip/ftx-tooltip.component.spec.ts
+++ ftx-tooltip/src/lib/ftx-tooltip/ftx-tooltip.component.spec.ts
@@ -18,4 +18,26 @@ describe('FtxTooltipComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('should assert text', () => {
+    it('should change the text', () => {
+      const divElement = fixture.nativeElement.querySelector(
+        '[data-test="text-tooltip"]'
+      );
+      component.text = 'test';
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(divElement.textContent).toEqual('test');
+      });
+    });
+
+    it('should change not the text', () => {
+      const divElement = fixture.nativeElement.querySelector(
+        '[data-test="text-tooltip"]'
+      );
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(divElement.textContent).toEqual('test');
+      });
+    });
+  });
 });
