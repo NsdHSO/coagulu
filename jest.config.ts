68bf3063acd9d4b4edff20a8a7415c9769c22c84
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-08-31 17:05:54 +0300

    chore:🌻 all coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,8 @@ import { getJestProjects } from '@nx/jest';
 
 export default {
   projects: getJestProjects(),
+  coverageDirectory: './coverage/coagulu',
+
+  // include cobertura in coverageReporters
+  coverageReporters: ['html', 'text', 'text-summary'],
 };
