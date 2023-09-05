ee00c8b13c1711adb4862faa3a4ed15e5eec8f02
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-09-05 07:00:15 +0300

    chore:🌻 add input component

---
+++ ftx-sivan-shared/src/lib/components/input/input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sivan-input',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+})
+export class InputComponent {}
