1154077292ea6df65166ac906225b560d1de5aa9
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-09-09 18:57:53 +0300

    chore:🌻 add currency input

--- src/app/components/contentSteps/spendMoney/spend-money.component.ts
+++ src/app/components/contentSteps/spendMoney/spend-money.component.ts
@@ -1,7 +1,11 @@
 import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
-import { GenerateFormBuilderService, TypeConstantEnum } from 'ngx-ftx-forms';
+import {
+  GenerateFormBuilderService,
+  TypeConstantEnum,
+  TypePattern,
+} from 'ngx-ftx-forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SivanInputComponent } from 'ngx-ftx-shared';
 
@@ -22,14 +26,39 @@ export class SpendMoneyComponent {
   @ViewChild('default') defaultTemplate!: TemplateRef<unknown>;
 
   private readonly _fb = inject(GenerateFormBuilderService);
-  form: any; //eslint-disable-line
+  simple: any; //eslint-disable-line
+  currency: any; //eslint-disable-line
   constructor() {
-    this.form = this._fb.buildFormFromJson({
+    this.simple = this._fb.buildFormFromJson({
       label: 'name',
       value: 'IVan',
       validators: [{ type: TypeConstantEnum.REQUIRED }],
       placeholder: 'Isvan Avramescu',
       labelHint: 'Name for User',
     });
+    this.currency = this._fb.buildFormFromJson({
+      label: 'personalData',
+      labelHint: 'Personal Data',
+      values: [
+        {
+          label: 'valueCurrency',
+          labelHint: 'Personal Nested',
+          value: '123141232311',
+          validators: [
+            {
+              type: TypeConstantEnum.PATTERN,
+              option: TypePattern.ONLY_NUMBER,
+              errorMsg: 'Only Integer Number',
+            },
+          ],
+        },
+        {
+          label: 'currency',
+          value: 'fa_solid:RO',
+          labelHint: 'Personal TEst',
+        },
+      ],
+    });
+    console.log(this.currency);
   }
 }
