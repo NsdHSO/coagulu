3c7187a87fc42267d388e88819eb57dd4ab6b5e7
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-09-11 00:43:49 +0300

    chore:🌻 add increment directive

---
+++ ftx-tooltip/src/lib/ftx-tooltip/directive/increment-zindex.directive.spec.ts
@@ -0,0 +1,8 @@
+import { IncrementZindexDirective } from './increment-zindex.directive';
+
+describe('IncrementZindexDirective', () => {
+  it('should create an instance', () => {
+    const directive = new IncrementZindexDirective();
+    expect(directive).toBeTruthy();
+  });
+});
