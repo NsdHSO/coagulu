6947d3e406107c7137ddac6fde3ceb072437e493
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-09-12 09:16:21 +0300

    chore:🌻 add some spec

--- ftx-tooltip/src/lib/ftx-tooltip/directive/increment-zindex.directive.spec.ts
+++ ftx-tooltip/src/lib/ftx-tooltip/directive/increment-zindex.directive.spec.ts
@@ -1,8 +1,27 @@
 import { IncrementZindexDirective } from './increment-zindex.directive';
+import { TestBed } from '@angular/core/testing';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('IncrementZindexDirective', () => {
+  const setupSpy = (elementRef: unknown, renderer2: unknown) => {
+    return TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ElementRef,
+          useValue: elementRef,
+        },
+        {
+          provide: Renderer2,
+          useValue: renderer2,
+        },
+        IncrementZindexDirective,
+      ],
+    }).inject(IncrementZindexDirective);
+  };
   it('should create an instance', () => {
-    const directive = new IncrementZindexDirective();
+    const elementRef = {};
+    const renderer2 = {};
+    const directive = setupSpy(elementRef, renderer2);
     expect(directive).toBeTruthy();
   });
 });
--- ftx-tooltip/src/lib/ftx-tooltip/directive/increment-zindex.directive.ts
+++ ftx-tooltip/src/lib/ftx-tooltip/directive/increment-zindex.directive.ts
@@ -1,17 +1,32 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  inject,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[coaguluIncrementZindex]',
   standalone: true,
 })
 export class IncrementZindexDirective implements OnInit {
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  /**
+   * {ElementRef} elementRef - Reference to the host element.
+   * @private
+   */
+  private readonly elementRef = inject(ElementRef);
+  /**
+   * {Renderer2} renderer - Renderer to manipulate the DOM.
+   * @private
+   */
+  private readonly renderer2 = inject(Renderer2);
 
   ngOnInit() {
     // Find all elements with the 'incrementZIndex' directive and increment their z-index by 10
     const elements = document.querySelectorAll('[coaguluIncrementZindex]');
     elements.forEach((element, index) => {
-      this.renderer.setStyle(element, 'z-index', 100 - index);
+      this.renderer2.setStyle(element, 'z-index', 100 - index);
     });
   }
 }
