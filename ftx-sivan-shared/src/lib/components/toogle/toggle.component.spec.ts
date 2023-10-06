c612122baab1ad27387ee5c8fc975eb1b1dfd7bb
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-10-06 20:02:28 +0300

    test:🐳 all test when have icon

--- ftx-sivan-shared/src/lib/components/toogle/toggle.component.spec.ts
+++ ftx-sivan-shared/src/lib/components/toogle/toggle.component.spec.ts
@@ -1,6 +1,8 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { IconCoreModule } from 'ngx-liburg-icon';
 import { ToggleComponent } from './toggle.component';
 
 describe('ToogleComponent', () => {
@@ -9,7 +11,7 @@ describe('ToogleComponent', () => {
   let formControl = new FormControl('TESTr');
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ToggleComponent],
+      imports: [ToggleComponent, MatIconModule, IconCoreModule],
     }).compileComponents();
     fixture = TestBed.createComponent(ToggleComponent);
     component = fixture.componentInstance;
@@ -66,6 +68,76 @@ describe('ToogleComponent', () => {
         wrapperElement.click();
         expect(spy).toHaveBeenCalled();
         expect(component.control.value).toBeFalsy();
+      });
+    });
+    describe('should label go', () => {
+      it('should left', () => {
+        component.control = new FormControl(true);
+        fixture.detectChanges();
+        const wrapperElement = fixture.nativeElement.querySelector(
+          '[data-test="shared-toggle-type-input-label"]'
+        );
+        expect(wrapperElement.classList.contains('translate-x-3/4')).toBe(true);
+      });
+      it('should right', () => {
+        component.control = new FormControl(false);
+        fixture.detectChanges();
+        const wrapperElement = fixture.nativeElement.querySelector(
+          '[data-test="shared-toggle-type-input-label"]'
+        );
+        expect(wrapperElement.classList.contains('-translate-x-4')).toBe(true);
+      });
+    });
+    describe('should check content', () => {
+      describe('be implicit', () => {
+        it('should not have value use implicit value', () => {
+          component.control = new FormControl(true);
+          component.valuesToggled = null;
+          fixture.detectChanges();
+          const wrapperElement = fixture.nativeElement.querySelector(
+            '[data-test="shared-toggle-type-input-label-implicit"]'
+          );
+          expect(wrapperElement.innerHTML).toBe('Yes');
+        });
+        it('should not have value use implicit value', () => {
+          component.control = new FormControl(false);
+          component.valuesToggled = null;
+          fixture.detectChanges();
+          const wrapperElement = fixture.nativeElement.querySelector(
+            '[data-test="shared-toggle-type-input-label-implicit"]'
+          );
+          expect(wrapperElement.innerHTML).toBe('No');
+        });
+      });
+      describe('have another value', () => {
+        it('should have set icon when control have value', () => {
+          component.control = new FormControl(false);
+          component.valuesToggled = [
+            { icon: 'fa_solid:CR', description: '', value: true },
+            { icon: 'fa_solid:RO', description: 'test', value: false },
+          ];
+          fixture.detectChanges();
+          const wrapperElement = fixture.nativeElement.querySelector(
+            '[data-test="shared-toggle-type-input-label-another"]'
+          );
+          expect(wrapperElement.getAttribute('ng-reflect-svg-icon')).toBe(
+            'fa_solid:RO'
+          );
+        });
+        it('should have set icon when no control have value', () => {
+          component.control = new FormControl(true);
+          component.valuesToggled = [
+            { icon: 'fa_solid:CR', description: '', value: true },
+            { icon: 'fa_solid:RO', description: 'test', value: false },
+          ];
+          fixture.detectChanges();
+          const wrapperElement = fixture.nativeElement.querySelector(
+            '[data-test="shared-toggle-type-input-label-another"]'
+          );
+          expect(wrapperElement.getAttribute('ng-reflect-svg-icon')).toBe(
+            'fa_solid:CR'
+          );
+        });
       });
     });
     it('should check if the correct class is applied when is not disabled', () => {
