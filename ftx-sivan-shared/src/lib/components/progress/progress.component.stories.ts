a5f564a2e6ca5e5107237d826772bf498d878943
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-11-03 17:16:11 +0200

    chore:🌻 init the storybook for progress bar

--- ftx-sivan-shared/src/lib/components/progress/progress.component.stories.ts
+++ ftx-sivan-shared/src/lib/components/progress/progress.component.stories.ts
@@ -8,18 +8,56 @@ import {
   Story,
 } from '@storybook/angular';
 import { ProgressComponent } from './progress.component';
-import { of } from 'rxjs';
+import { interval, map, of, startWith } from 'rxjs';
+import { ActivatedRoute, provideRouter } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { IconCoreModule } from 'ngx-liburg-icon';
+import { AsyncPipe } from '@angular/common';
 
 export default {
   title: 'ProgressComponent',
   component: ProgressComponent,
   decorators: [
     moduleMetadata({
-      imports: [ProgressComponent],
-      providers: [provideAnimations()],
+      imports: [ProgressComponent, MatIconModule, IconCoreModule, AsyncPipe],
+      providers: [
+        provideAnimations(),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {
+                stepper: [{ icon: 'fa_solid:RO' }, { icon: 'fa_solid:CR' }],
+                actions: [
+                  { disable: of(true), name: 'Pause' },
+                  {
+                    disable: (() =>
+                      interval(2500).pipe(
+                        map((v) => v % 2 === 0),
+                        startWith(false)
+                      ))(),
+                    name: 'Continue',
+                  },
+                ],
+              } as unknown,
+            },
+          } as ActivatedRoute as never,
+        },
+      ],
     }),
     applicationConfig({
-      providers: [importProvidersFrom(HttpClientModule)],
+      providers: [
+        importProvidersFrom(HttpClientModule),
+        provideRouter([
+          {
+            path: '',
+            loadComponent: () =>
+              import('./../input/sivan-input.component').then(
+                (c) => c.SivanInputComponent
+              ),
+          },
+        ]),
+      ],
     }),
   ],
   tags: ['autodocs'],
